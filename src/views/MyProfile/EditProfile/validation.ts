d224302b8725dd46f44ea63a0746b1ad99364a3b
nandimandalamsunny-rootquotient <nandimandalam.sunny@rootquotient.com>
2025-12-26 09:52:14 +0530

    fix(meeting): 543 493 610 605 601 600 fixes

--- src/views/MyProfile/EditProfile/validation.ts
+++ src/views/MyProfile/EditProfile/validation.ts
@@ -1,25 +1,43 @@
 import { REGEX } from "src/constants/regex";
 import * as Yup from "yup";
 
+const MAX_LENGTH = 50;
+
+const validationMsgs = {
+  FIRST_NAME: "First name is required",
+  LAST_NAME: "Last name is required",
+  PHONE_NUMBER: "Invalid phone number",
+  FIRST_NAME_MAX_LENGTH: "First name cannot exceed 50 characters",
+  LAST_NAME_MAX_LENGTH: "Last name cannot exceed 50 characters",
+  ONLY_LETTERS_ALLOWED: "Only letters are allowed",
+};
+
+const {
+  FIRST_NAME,
+  LAST_NAME,
+  PHONE_NUMBER,
+  FIRST_NAME_MAX_LENGTH,
+  LAST_NAME_MAX_LENGTH,
+  ONLY_LETTERS_ALLOWED,
+} = validationMsgs;
+
 export const editProfileValidation = Yup.object({
   firstName: Yup.string()
-    .required("First name is required")
-    .matches(REGEX.LETTERS_WITH_SPACES, "Only letters are allowed")
-    .max(50),
+    .required(FIRST_NAME)
+    .matches(REGEX.LETTERS_WITH_SPACES, ONLY_LETTERS_ALLOWED)
+    .max(MAX_LENGTH, FIRST_NAME_MAX_LENGTH),
 
   lastName: Yup.string()
-    .required("Last name is required")
-    .matches(REGEX.LETTERS_WITH_SPACES, "Only letters are allowed")
-    .max(50),
+    .required(LAST_NAME)
+    .matches(REGEX.LETTERS_WITH_SPACES, ONLY_LETTERS_ALLOWED)
+    .max(MAX_LENGTH, LAST_NAME_MAX_LENGTH),
 
   email: Yup.string().nullable().email(),
-  phoneNumber: Yup.string()
-    .nullable()
-    .matches(REGEX.DIGITS, "Invalid phone number"),
+  phoneNumber: Yup.string().nullable().matches(REGEX.DIGITS, PHONE_NUMBER),
   countryCode: Yup.string().nullable(),
   profilePicture: Yup.string().nullable(),
   address: Yup.string().nullable(),
-  contactNumber: Yup.string().nullable(),
+  contactNumber: Yup.string().nullable().matches(REGEX.DIGITS, PHONE_NUMBER),
   attachmentId: Yup.string().nullable(),
   emailId: Yup.string().nullable(),
   id: Yup.string().nullable(),
