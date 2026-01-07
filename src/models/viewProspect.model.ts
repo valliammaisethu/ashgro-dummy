2d3ae9359a7d74afff23d4f4b852679688e4f51a
nandimandalamsunny-rootquotient <nandimandalam.sunny@rootquotient.com>
2026-01-07 10:50:26 +0530

    fix(755): added additional comments in the prospects details

--- src/models/viewProspect.model.ts
+++ src/models/viewProspect.model.ts
@@ -61,6 +61,9 @@ export class ViewProspect {
   @serializable
   initiationFee = "";
 
+  @serializable
+  additionalComments = "";
+
   @serializable(list(object(ActivityDetails)))
   activityDetails: ActivityDetails[] = [];
 }
