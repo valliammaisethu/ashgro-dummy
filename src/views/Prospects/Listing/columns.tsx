4ed0981906afbc0da1f81ca11e7a63f93859efc6
nandimandalamsunny-rootquotient <nandimandalam.sunny@rootquotient.com>
2026-01-12 09:19:18 +0530

    feat(listing): 760 remove status dropdown

--- src/views/Prospects/Listing/columns.tsx
+++ src/views/Prospects/Listing/columns.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { ColumnsType } from "antd/es/table";
+import { Select } from "antd";
 import dayjs from "dayjs";
 
 import { ProspectsList } from "src/models/prospects.model";
 import PhoneNumberLabel from "src/shared/components/Table/atoms/PhoneNumberLabel";
-import StatusDropdown from "src/shared/components/StatusDropdown";
 import { stopPropagation } from "src/shared/utils/eventUtils";
 import { LeadStatus } from "src/models/meta.model";
 import ProspectFollowUpDate from "./Components/ProspectFollowUpDate";
-
 import RowActions from "src/shared/components/Table/atoms/RowActions";
+import {
+  selectStatus,
+  selectStatusClassName,
+} from "src/constants/sharedComponents";
 
 interface ColumnProps {
   handleOnEdit: (prospect: ProspectsList) => void;
   handleOnDelete: (prospect: ProspectsList) => void;
   leadStatusOptions: LeadStatus[];
-  onStatusChange: (prospectId: string, leadStatusId: string) => void;
+  onStatusChange: (prospectId?: string, leadStatusId?: string) => void;
   updatingProspectId?: string;
   onFollowUpDateChange: (
     prospectId: string,
@@ -37,7 +40,7 @@ export const getColumns = ({
     title: "Phone Number",
     dataIndex: "phone",
     key: "phone",
-    width: "13%",
+    width: "10%",
     render: (_, record) => (
       <PhoneNumberLabel phoneNumber={record?.contactNumber} />
     ),
@@ -55,30 +58,35 @@ export const getColumns = ({
     ),
   },
   {
-    title: "Inquiry Date",
-    dataIndex: "inquiryDate",
-    key: "inquiryDate",
-    width: "10%",
-  },
-  {
     title: "Lead Source",
     dataIndex: "leadSource",
     key: "leadSource",
     width: "10%",
   },
   {
+    title: "Inquiry Date",
+    dataIndex: "inquiryDate",
+    key: "inquiryDate",
+    width: "10%",
+  },
+  {
     title: "Lead Status",
     dataIndex: "status",
     key: "status",
-    width: "10%",
+    width: "13%",
     render: (status, record) => (
       <div onClick={stopPropagation}>
-        <StatusDropdown
+        <Select
           value={record.leadStatus ?? status}
-          options={leadStatusOptions || []}
-          onChange={(newStatus) => onStatusChange(record.id!, newStatus)}
+          options={leadStatusOptions?.map((opt) => ({
+            label: opt.statusName,
+            value: opt.id,
+          }))}
+          onChange={(newStatus) => onStatusChange(record?.id, newStatus)}
           loading={updatingProspectId === record.id}
+          className={selectStatusClassName}
           onClick={stopPropagation}
+          placeholder={selectStatus}
         />
       </div>
     ),
--- src/views/StaffMembers/Listing/index.tsx
+++ src/views/StaffMembers/Listing/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 import Header from "./Header";
@@ -6,22 +6,16 @@ import {
   StaffMemberDetails,
   StaffMembersListingParams,
 } from "src/models/staffMember.model";
-import Form from "src/shared/components/Form";
+import Table from "src/shared/components/Table";
+import { getColumns } from "./columns";
 
-import styles from "./staffMemberListing.module.scss";
-import ListHeader from "src/shared/components/atoms/Table/Profile/ListHeader";
-import { clulbHeaderGrid, staffMemberHeaders } from "../constants";
-import ConditionalRender from "src/shared/components/ConditionalRender";
 import { StaffMembersService } from "src/services/StaffMembersService/staffMembers.service";
-import Profile from "src/shared/components/atoms/Table/Profile";
-import Actions from "src/shared/components/atoms/Table/Actions";
 import useRedirect from "src/shared/hooks/useRedirect";
 import { SettingFormModalModel } from "src/views/Settings/constants";
 import StaffMembersForm from "../StaffMembersForm";
 import DeleteModal from "../DeleteModal";
 import { VisibilityType } from "src/enums/visibilityType.enum";
-import Pagination from "src/shared/components/Pagination";
-import { fillEmptyData } from "src/shared/utils/helpers";
+
 interface ModalState {
   open: boolean;
   mode: SettingFormModalModel;
@@ -50,12 +44,12 @@ const StaffMembersListing = () => {
 
   const { getStaffMembersList } = StaffMembersService();
 
-  const { data, isFetching, isSuccess } = useQuery(
-    getStaffMembersList(queryParams),
-  );
+  const { data, isFetching } = useQuery(getStaffMembersList(queryParams));
 
-  const handleNavigateToDetails = (id?: string) => () =>
-    navigateToStaffMemberDetails(id);
+  const handleNavigateToDetails = useCallback(
+    (id?: string) => () => navigateToStaffMemberDetails(id),
+    [navigateToStaffMemberDetails],
+  );
 
   const [modalState, setModalState] = useState<ModalState>({
     open: false,
@@ -69,6 +63,23 @@ const StaffMembersListing = () => {
     setModalState({ open: !!mode, mode, item });
   };
 
+  const columns = useMemo(
+    () =>
+      getColumns({
+        handleOnEdit: (item) =>
+          handleModalVisibility(VisibilityType.EDIT, item),
+        handleOnDelete: (item) => setDeleteItem(item),
+      }),
+    [],
+  );
+
+  const handleRowClick = useCallback(
+    (record: StaffMemberDetails) => ({
+      onClick: handleNavigateToDetails(record.id),
+    }),
+    [handleNavigateToDetails],
+  );
+
   return (
     <div>
       <Header
@@ -76,70 +87,22 @@ const StaffMembersListing = () => {
         onStaffAdd={() => handleModalVisibility(VisibilityType.ADD)}
         onSearch={handleSearch}
       />
-      <Form>
-        <div className={styles.listContainer}>
-          <ListHeader
-            columnTemplate={clulbHeaderGrid}
-            headers={staffMemberHeaders}
-          />
+      <Table<StaffMemberDetails>
+        columns={columns}
+        dataSource={data?.staffs}
+        currentPage={data?.pagination?.currentPage ?? queryParams.page}
+        totalPages={data?.pagination?.overallPages}
+        onPageChange={handlePageChange}
+        hasData={!!data?.staffs?.length}
+        onRow={handleRowClick}
+        loading={isFetching}
+      />
 
-          <ConditionalRender
-            records={data?.staffs}
-            isPending={isFetching}
-            isSuccess={isSuccess}
-          >
-            <div className={styles.body}>
-              {data?.staffs?.map((item) => {
-                return (
-                  <div
-                    key={item.id}
-                    className={styles.rowContainer}
-                    onClick={handleNavigateToDetails(item?.id)}
-                  >
-                    <Profile
-                      firstName={item.firstName}
-                      lastName={item.lastName}
-                      email={item.email}
-                      profilePictureUrl={item.profilePictureUrl}
-                    />
-
-                    <div className={styles.rowItem}>
-                      {fillEmptyData(item?.staffDepartment)}
-                    </div>
-                    <div className={styles.rowItem}>
-                      {fillEmptyData(item?.title)}
-                    </div>
-
-                    <Actions
-                      showSelect={false}
-                      onEditClick={() =>
-                        handleModalVisibility(VisibilityType.EDIT, item)
-                      }
-                      onDeleteClick={() => setDeleteItem(item)}
-                    />
-                  </div>
-                );
-              })}
-            </div>
-            <Pagination
-              currentPage={
-                data?.pagination?.currentPage ?? queryParams.page ?? 1
-              }
-              totalPages={data?.pagination?.overallPages ?? 1}
-              onPageChange={handlePageChange}
-              hasData={!!data?.staffs && data.staffs.length > 0}
-            />
-          </ConditionalRender>
-        </div>
-      </Form>
-
-      {
-        <DeleteModal
-          visible={!!deleteItem?.id}
-          toggleVisibility={() => setDeleteItem(undefined)}
-          staffMember={deleteItem}
-        />
-      }
+      <DeleteModal
+        visible={!!deleteItem?.id}
+        toggleVisibility={() => setDeleteItem(undefined)}
+        staffMember={deleteItem}
+      />
       {modalState.open && (
         <StaffMembersForm
           isOpen={modalState.open}
